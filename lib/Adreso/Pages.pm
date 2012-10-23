bdaa8131fd2e0fc175a6a2196427460cdb7085f7
jun <jun@www22241ue.(none)>
2012-10-24 00:28:55 +0900

    住所正規化のロジックを削除

--- lib/Adreso/Pages.pm
+++ lib/Adreso/Pages.pm
@@ -4,7 +4,6 @@ use warnings;
 use Data::Printer;
 use Data::Dumper;
 use Encode;
-use WebService::Simple;
 use URI::Escape;
 use JSON;
 use utf8;
@@ -20,7 +19,6 @@ sub index {
 
     # 住所の時は正規化APIを叩く
     if ( $query->{type} eq 'addr' ) {
-        #$page->{addr_normalized} = &addr_normalize( $query->{parts}[0] );
         $page->{addr_normalized} = $query->{parts}[0];
     }
     elsif ( $query->{type} eq 'latlon' ) {
@@ -50,33 +48,6 @@ sub query_parse {
     return $query_obj;
 }
 
-sub addr_normalize {
-    my ($addr_raw) = @_;
-    my $addr_normalize = $addr_raw;
-
-    my $normalize = WebService::Simple->new(
-        base_url        => 'https://api.loctouch.com/v1/geo/address_normalize',
-        response_parser => {
-	    module => 'JSON',
-	    args => {
-	        json => JSON->new->utf8,
-	    }
-        },
-    );
-
-    eval {
-        my $res = $normalize->get( { address => $addr_raw } );
-        my $json = $res->parse_response;
-        if ( $json && $json->{code} == 200 ) {
-            $addr_normalize = $json->{result}->{address};
-        }
-    };
-    if ($@) {
-        p $@;
-    }
-
-    return $addr_normalize;
-}
 
 1
 __END__
